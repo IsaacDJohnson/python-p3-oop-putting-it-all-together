aa015223809defb090676859441ebffb18237460
IsaacDJohnson <isaac.designws@gmail.com>
2024-01-22 13:48:02 -0600

    completed lesson

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python3
 
 class Book:
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def __init__(self, title="title", page_count="count"):
+        self.title = title
+        self.page_count = page_count
+        
+    @property
+    def title(self):
+        """The title property"""
+        return self._title
+
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+        pass
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
+
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) is int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+        pass
+
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class Shoe:
+
+    def __init__(self, brand ="brand", size= 0, condition = ""):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    @property
+    def brand(self):
+        """The brand property"""
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        """the brand setter"""
+        self._brand = brand
+
+    @property
+    def size(self):
+        """the size property"""
+        return self._size
+        
+
+    @size.setter
+    def size(self, size):
+        """the size setter"""
+        if type(size) is int:
+            self._size = size
+        else:
+            print("size must be an integer") 
+
+
     pass
